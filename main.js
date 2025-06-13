b97b25b5ef4a3533391fbc8a19149c6636073b46
daveyedgar <105902240+daveyedgartanibulie@users.noreply.github.com>
2025-06-14 00:11:41 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// Button click handler with accessible alert
+document.getElementById('actionBtn').addEventListener('click', () => {
+    alert('Hello! Thank you for visiting this simple website.');
+  });
